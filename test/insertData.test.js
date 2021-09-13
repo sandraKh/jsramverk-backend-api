5c93d2ea950fae98d82b370b7d558acd59d5a3d3
Sandra Kullar <sandra.kullar@gmail.com>
2021-09-13 11:03:51 +0200

    kmom02

--- test/insertData.test.js
+++ test/insertData.test.js
@@ -1,32 +1,32 @@
-// const   expect      = require('chai').expect,
-//         request     = require('supertest'),
-//         basicSetup  = require('./helper/basicSetup'),
-//         app         = require('../app');
+const   expect      = require('chai').expect,
+        request     = require('supertest'),
+        basicSetup  = require('./helper/basicSetup'),
+        app         = require('../app');
 
 
-// describe('POST: / route to insert data', ()=>{
+describe('POST: / route to insert data', ()=>{
     
-//     basicSetup();
+    basicSetup();
 
-//     it('Data is posted and is valid', (done)=>{
-//         let toSendData = { title:'jemmy doe', content: 'computer science'} 
-//         request(app).post('/')
-//             .send(toSendData)
-//             .then((res)=>{
-//                 expect(res.statusCode).to.equal(200);
-//                 expect(res.body).to.include(toSendData);
-//                 done();
-//             })
-//             .catch((err) => done(err))
-//     })
+    it('Data is posted and is valid', (done)=>{
+        let toSendData = { title:'jemmy doe', content: 'computer science'} 
+        request(app).post('/')
+            .send(toSendData)
+            .then((res)=>{
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.include(toSendData);
+                done();
+            })
+            .catch((err) => done(err))
+    })
 
-//     it('no content field given', (done)=>{
-//         request(app).post('/')
-//             .send({title:'john doe'})
-//             .then((res)=>{
-//                 expect(res.statusCode).to.equal(404)
-//                 done()
-//             })
-//             .catch((err) => done(err))
-//     })
-// })
+    it('no content field given', (done)=>{
+        request(app).post('/')
+            .send({title:'john doe'})
+            .then((res)=>{
+                expect(res.statusCode).to.equal(404)
+                done()
+            })
+            .catch((err) => done(err))
+    })
+})
