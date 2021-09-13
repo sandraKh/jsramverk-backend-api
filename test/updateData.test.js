ee525285f9662683a8a02962c54277f49c8cc38b
Sandra Kullar <sandra.kullar@gmail.com>
2021-09-13 12:03:43 +0200

    Revert "fix"
    This reverts commit 6b359d05fbf868a98133d708ceef96c5b3775b65.

--- test/updateData.test.js
+++ test/updateData.test.js
@@ -1,35 +1,35 @@
-// const   expect      = require('chai').expect,
-//         request     = require('supertest'),
-//         basicSetup  = require('./helper/basicSetup'),
-//         app         = require('../app')
+const   expect      = require('chai').expect,
+        request     = require('supertest'),
+        basicSetup  = require('./helper/basicSetup'),
+        app         = require('../app')
 
 
-// describe('PATCH: /:id route to update data', () => {
-//     basicSetup();
+describe('PATCH: /:id route to update data', () => {
+    basicSetup();
 
-//     it('Updating existing data', (done) => {
-//         let toSendData = { title:'test doc', content: 'test'} 
-//         request(app).post('/')
-//             .send(toSendData)
-//             .then((res)=>{
-//                 request(app).patch('/' + res.body._id)
-//                 .send({ content: 'updated branch'})
-//                 .then((res2) => {
-//                     expect(res2.statusCode).to.equal(200)
-//                     expect(res2.body).to.include({ modifiedCount: 1 })
-//                     done()
-//                 })
-//                 .catch((err) => done(err))
-//                 })
-//             .catch((err) => done(err)) 
-//     })
+    it('Updating existing data', (done) => {
+        let toSendData = { title:'test doc', content: 'test'} 
+        request(app).post('/')
+            .send(toSendData)
+            .then((res)=>{
+                request(app).patch('/' + res.body._id)
+                .send({ content: 'updated branch'})
+                .then((res2) => {
+                    expect(res2.statusCode).to.equal(200)
+                    expect(res2.body).to.include({ modifiedCount: 1 })
+                    done()
+                })
+                .catch((err) => done(err))
+                })
+            .catch((err) => done(err)) 
+    })
 
-//     it('updating from non existent data', (done) => {
-//         request(app).patch('/123')
-//                 .then((res) => {
-//                     expect(res.statusCode).to.equal(404);
-//                     done()
-//                 })
-//                 .catch((err) => done(err))
-//     })
-// })
+    it('updating from non existent data', (done) => {
+        request(app).patch('/123')
+                .then((res) => {
+                    expect(res.statusCode).to.equal(404);
+                    done()
+                })
+                .catch((err) => done(err))
+    })
+})
